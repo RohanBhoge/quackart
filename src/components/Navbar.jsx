55ad4412e5ff6cd7cd6d54a16f08d6f815dfef5f
Rohan <bhogerohan12@gmail.com>
2024-09-26 14:17:39 +0530

    add starting click

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import profile from "../assets/profile.svg";
 import cart from "../assets/cart.svg";
 import "./Navbar.css";
@@ -8,7 +8,7 @@ import { Link, useNavigate } from "react-router-dom";
 function Navbar() {
   const { logedUser = {} } = useContext(ProductContext); // Destructure with a default empty object
   const navigate = useNavigate();
-
+  const [activeSpan, setActiveSpan] = useState(true);
   // Calculate total items in the cart
   const totalCart = logedUser.cartproducts
     ? logedUser.cartproducts.reduce((total, item) => total + item.quantity, 0)
@@ -16,11 +16,20 @@ function Navbar() {
 
   return (
     <div className="navbar">
-      <p onClick={() => navigate("/")} className="logo">
-        QuickCart
+      <p
+        onClick={() => {
+          navigate("/");
+          setActiveSpan(false);
+        }}
+        className="logo"
+      >
+        QuickCart{" "}
+        <span className={activeSpan ? "" : "span-click"}>
+          {" "}
+          Click here to continue
+        </span>
       </p>
       <div className="right">
-        
         {/* Link to login/logout based on logged-in status */}
         <Link
           to={Object.keys(logedUser).length !== 0 ? "/logout" : "/login"}
