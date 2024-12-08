d160f77e794497efa707a7227cb92496645c9e77
Rohan <bhogerohan12@gmail.com>
2024-12-08 22:53:44 +0530

    vite file updated

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/quackart/",
-  server:{port:5173}
+  esbuild: {
+    loader: "jsx", // Tell esbuild to treat .js files as JSX
+    include: [/\.js$/], // Apply the JSX loader to .js files
+  },
 });
