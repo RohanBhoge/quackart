616ef4dbe814f6eb179774c2f82f3c1c53a015a2
Rohan <bhogerohan12@gmail.com>
2024-12-08 23:04:40 +0530

    set type module

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    loader: "jsx", // Tell esbuild to treat .js files as JSX
-    include: [/\.js$/], // Apply the JSX loader to .js files
-  },
+  base: "/quackart/",
+  server: { port: 5173 },
 });
